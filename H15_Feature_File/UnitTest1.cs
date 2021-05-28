7a5fa9c494f8d65a7477c18bc8a6db722d9aeba4
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2021-05-28 20:10:09 +0300

    Добавьте файлы проекта.

---
+++ H15_Feature_File/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace H15_Feature_File
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
